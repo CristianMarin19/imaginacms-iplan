d0739f52e92bd547f03473e1adc8b43848ef5d95
Slim Arias Sanhez <slimarias@github.com>
2021-02-12 19:53:04 -0500

    +UPDATE: category title in entity

--- Entities/CategoryTranslation.php
+++ Entities/CategoryTranslation.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryTranslation extends Model
 {
     public $timestamps = false;
-    protected $fillable = ["name", "slug", "description"];
+    protected $fillable = ["title", "slug", "description"];
     protected $table = 'iplan__category_translations';
 }
