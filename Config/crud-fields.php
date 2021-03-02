24f86acfda39ed85c4bc8919146d4cb0de1e05d2
Slim Arias Sanhez <slimarias@github.com>
2021-03-02 18:57:23 -0500

    +ADD: plan productId crud field

---
+++ Config/crud-fields.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'plans' => [
+        'productId' => [
+            'loadOptions' => [
+                'apiRoute' => 'apiRoutes.qcommerce.products',
+                'select' => [ 'label' => 'name', 'id' => 'id' ],
+            ],
+            'value' => null, //If the field is not multiple, it must be null. Else, it must be an empty array
+            'type' => 'select', //It's recommended to use select or multiselect field types
+            'props' => [
+                'label' => 'Producto',
+                'multiple' => false,
+                'useChips' => true,
+
+            ],
+        ],
+    ]
+];
