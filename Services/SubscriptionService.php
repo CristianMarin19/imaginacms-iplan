f6a19dcea3a70b1076c568d4fd29d2b3fa5ec0c1
Slim Arias Sanhez <slimarias@github.com>
2021-05-12 15:37:55 -0500

    +ADD: subscription service

---
+++ Services/SubscriptionService.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace Modules\Iplan\Services;
+use Modules\Iplan\Entities\Subscription;
+
+
+class SubscriptionService
+{
+
+    public function validate($model)
+    {
+        //Get entity attributes
+
+        $userDriver = config('asgard.user.config.driver');
+
+        $entityNamespace = get_class($model);
+        $entityNamespaceExploded = explode('\\', strtolower($entityNamespace));
+        $moduleName = $entityNamespaceExploded[1];//Get module name
+        $entityName = $entityNamespaceExploded[3];//Get entity name
+        //Get current full date
+        $now = Carbon::now()->format('Y-m-d h:i:s');
+        $subscription = Subscription::whereHas('limits', function ($q) use ($entityNamespace) {
+            //filter limits
+            $q->where('entity', $entityNamespace);
+        })->whereDate('end_date', '>', $now)->whereDate('start_date', '<=', $now)->where(function ($query) use ($userDriver){
+            $query->whereNull('entity')->orWhere(function ($query) use ($userDriver){
+                $query->where('entity_id', auth()->user()->id)->where('entity', "Modules\\User\\Entities\\{$userDriver}\\User");
+            });
+        })->where('status',1)
+            ->orderBy('id')
+            ->first();
+        if(!empty($subscription)) {
+            $limitsDisabled = 0;
+            $subLimits = $subscription->limits;
+            foreach($subLimits as $limit) {
+                $validateLimit = true;
+                $modelValue = null;
+                $limitAttribute = $limit->attribute; //get limit attribute name
+                //Validate if limit has attribute
+                if (!empty($limitAttribute) && isset($model->$limitAttribute)) {
+                    $modelValue = (string)$model->$limitAttribute ?? null;
+                    if ($modelValue != $limit->attribute_value) $validateLimit = false;
+                }
+                if ($validateLimit) {
+                    if((int)$limit->quantity > 0) {
+                        if ((int)$limit->quantity_used >= (int)$limit->quantity) {
+                            $limitsDisabled++;
+                        }
+                    }
+                }
+            }
+            if($limitsDisabled==count($subLimits)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
