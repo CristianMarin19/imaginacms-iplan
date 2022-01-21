32e76124702cd7e13c67387873d777c2b3a1c560
Michael <msolano@imaginacolombia.com>
2022-01-20 19:42:45 -0500

    +Update: Model Plan, add trait `WithProduct`

--- Entities/Plan.php
+++ Entities/Plan.php
@@ -4,10 +4,11 @@ namespace Modules\Iplan\Entities;
 
 use Astrotomic\Translatable\Translatable;
 use Modules\Core\Icrud\Entities\CrudModel;
+use Modules\Isite\Traits\WithProduct;
 
 class Plan extends CrudModel
 {
-    use Translatable;
+    use Translatable, WithProduct;
 
     protected $table = 'iplan__plans';
     public $transformer = 'Modules\Iplan\Transformers\PlanTransformer';
@@ -25,37 +26,37 @@ class Plan extends CrudModel
       "category_id",
       "options",
     ];
-  
+
   protected $casts = [
     'options' => 'array'
   ];
-  
+
   public function setOptionsAttribute($value)
   {
     $this->attributes['options'] = json_encode($value);
   }
-  
-  
+
+
   public function getOptionsAttribute($value)
   {
     return json_decode($value);
   }
-  
+
   public function limits()
   {
     return $this->belongsToMany(Limit::class, 'iplan__plan_limits');
   }
-  
+
   public function category()
   {
     return $this->belongsTo(Category::class, "category_id");
   }
-  
+
   public function categories()
   {
     return $this->belongsToMany(Category::class, 'iplan__plan_category');
   }
-  
+
   public function product()
   {
     return $this->morphOne("Modules\\Icommerce\\Entities\\Product", "entity");
