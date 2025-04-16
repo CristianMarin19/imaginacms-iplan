343ecdc3b76db106251028aa75f03aff6ac39a73
Cristian Marin <marin101998@gmail.com>
2025-04-16 14:40:23 -0500

    + fix | v10 16-04-25 add livewire component access validator

---
+++ Resources/views/frontend/livewire/access-validator.blade.php
@@ -0,0 +1,9 @@
+@if(!$hasSubscription)
+  <div class="d-flex justify-content-center">
+    <a href="{{$buttonHref}}" target="_self">
+      <button class="button-base button-base button-primary button-custom button-normal">
+        {{$buttonLabel}}
+      </button>
+    </a>
+  </div>
+@endif
